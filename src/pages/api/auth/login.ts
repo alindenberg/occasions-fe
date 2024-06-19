74e62b89988fa913f2bf14cecf10732f3760b5b7
Austin Lindenberg <austin@appsumo.com>
2024-06-19 07:50:54 -0700

    remove consoles

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -30,7 +30,6 @@ export default async function handler(
         res.setHeader('Set-Cookie', cookie);
         res.status(200).json({ "message": "Login successful" })
     } catch (error: any) {
-        console.log("error ", error)
         if (error.type === 'CredentialsSignin') {
             res.status(401).json({ error: 'Invalid credentials.' })
         } else {
--- src/pages/api/users/me.ts
+++ src/pages/api/users/me.ts
@@ -22,7 +22,6 @@ export default async function handler(
 
         res.status(200).json(await response.json())
     } catch (error: any) {
-        console.log("error ", error)
         if (error.type === 'CredentialsSignin') {
             res.status(401).json({ error: 'Invalid credentials.' })
         } else {
