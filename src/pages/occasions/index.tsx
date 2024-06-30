a41d39d1e5aa6df86d1e4c54480bb493f72051ee
Austin Lindenberg <austin@appsumo.com>
2024-06-30 16:32:18 -0700

    use next url env var in occasions index

--- src/pages/occasions/index.tsx
+++ src/pages/occasions/index.tsx
@@ -76,7 +76,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         return { props: { occasions: [], isAuthenticated: false } }
     }
 
-    const res = await fetch('http://localhost:3000/api/occasions/', {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/occasions/`, {
         headers: {
             'Authorization': authCookie
         }
