ed2352f7d03ab0fdad2393cadb8dd3c54a0ab228
Austin Lindenberg <austin@appsumo.com>
2024-07-07 10:59:48 -0700

    more dark mode

--- src/components/occasions/UpcomingOccasionsList.tsx
+++ src/components/occasions/UpcomingOccasionsList.tsx
@@ -25,7 +25,7 @@ export default function UpcomingOccasionsList({ occasions, deletionHandler, modi
             </div>
             :
             <div className="py-4">
-                <div className="text-center py-4 bg-gray-100 border border-orange-400 shadow-xl rounded-lg overflow-hidden">
+                <div className="dark:text-black text-center py-4 bg-gray-100 border border-orange-400 shadow-xl rounded-lg overflow-hidden">
                     <p>Well that&apos;s bizarre. You have no upcoming occasions.</p>
                     <p>Quick, add them before you forget!</p>
                     <div className="flex justify-center pt-2"><CreateOccasionBtn /></div>
