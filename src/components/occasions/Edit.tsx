cc732b05bb3ce9be6c2eb7eae9c385b1009140c7
Austin Lindenberg <austin@appsumo.com>
2024-07-20 11:31:01 -0700

    tweak submit color button

--- src/components/occasions/Edit.tsx
+++ src/components/occasions/Edit.tsx
@@ -92,7 +92,7 @@ export default function EditOccasionComponent({ occasion, formSubmitFunction }: 
             </div>
             <button
                 type="submit"
-                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition duration-300 ease-in-out"
+                className="mt-4 px-4 py-2 bg-orange-500 hover:bg-orange-700 text-white rounded transition duration-300 ease-in-out"
             >
                 Submit
             </button>
