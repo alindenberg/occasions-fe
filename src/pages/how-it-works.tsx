22fdbcf94502d1089847350a37578e9cf902ca7c
Austin Lindenberg <austin@appsumo.com>
2024-07-20 12:06:03 -0700

    examples on how-it-works page

--- src/pages/how-it-works.tsx
+++ src/pages/how-it-works.tsx
@@ -11,7 +11,7 @@ export default function HowItWorksPage() {
 
     return (
         <div className="dark:text-black flex flex-grow items-center justify-center">
-            <div className="m-2 p-10 bg-gray-100 border-2 border-orange-400">
+            <div className="m-2 p-10 bg-gray-100 border-2 border-orange-400 text-center">
                 <div className="text-3xl font-bold pb-4">Welcome to Occasion Alerts!</div>
                 <div className="text-xl underline">Here&apos;s how it works:</div>
                 <div className="pt-2 text-lg">
@@ -30,7 +30,7 @@ export default function HowItWorksPage() {
 
                 <div className="flex flex-col sm:flex-row items-center justify-center mt-8">
                     <div className="relative">
-                        <Image src="/background.jpg" alt="Step 1: Create the Occasion" width={200} height={200} />
+                        <Image src="/occasions/OccasionSetup.png" alt="Step 1: Create the Occasion" width={400} height={400} />
                         <div className="hidden sm:block absolute top-1/2 right-0 transform translate-x-full -translate-y-1/2">
                             <svg className="w-6 h-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
@@ -38,7 +38,7 @@ export default function HowItWorksPage() {
                         </div>
                     </div>
                     <div className="relative mt-4 sm:mt-0 sm:ml-4">
-                        <Image src="/background.jpg" alt="Step 2: Wait for the Occasion" width={200} height={200} />
+                        <Image src="/occasions/CreatedOccasion.png" alt="Step 2: Wait for the Occasion" width={400} height={400} />
                         <div className="hidden sm:block absolute top-1/2 right-0 transform translate-x-full -translate-y-1/2">
                             <svg className="w-6 h-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path>
@@ -46,7 +46,7 @@ export default function HowItWorksPage() {
                         </div>
                     </div>
                     <div className="mt-4 sm:mt-0 sm:ml-4">
-                        <Image src="/background.jpg" alt="Step 3: Receive the email on the Occasion's date" width={200} height={200} />
+                        <Image src="/occasions/OccasionEmail.png" alt="Step 3: Receive the email on the Occasion's date" width={400} height={400} />
                     </div>
                 </div>
 
