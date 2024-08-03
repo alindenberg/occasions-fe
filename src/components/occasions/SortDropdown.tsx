878e1e7526a4c74c3ab9ee58ca202e6caa43193f
Austin Lindenberg <austin@appsumo.com>
2024-08-03 12:02:24 -0700

    Make sort a button rather than dropdown for now

--- src/components/occasions/SortDropdown.tsx
+++ src/components/occasions/SortDropdown.tsx
@@ -24,38 +24,12 @@ export default function OccasionsFilterDropdown({ onClick }: { onClick: Function
 
     return (
         <div className="relative">
-            <div className="inline-block" onMouseEnter={handleOpenDropdown} onMouseLeave={handleCloseDropdown}>
-                <button
-                    className="py-2 px-4 bg-gray-100 border border-orange-400 bg-white rounded-md shadow-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent flex items-center"
-                >
-                    <span className="md:text-xl font-bold">{viewingDescending ? 'Date ↓' : 'Date ↑'}</span>
-                    {/* <FontAwesomeIcon className="ml-2 self-center" icon={faCaretDown} /> */}
-                </button>
-                {isOpen && (
-                    <div
-                        className="absolute z-10 pt-2 w-48 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5"
-                        onMouseLeave={handleCloseDropdown}
-                    >
-                        <button
-                            onClick={() => handleSelect(OCCASION_SORTS.DATE_DESCENDING)}
-                            className={`block w-full text-left px-4 py-2 text-gray-700 ${viewingDescending ? 'bg-gray-200 cursor-not-allowed' : 'hover:bg-gray-100'
-                                }`}
-                            disabled={viewingDescending}
-                        >
-                            Date ↓
-                        </button>
-                        <button
-                            onClick={() => handleSelect(OCCASION_SORTS.DATE_ASCENDING)}
-                            className={`block w-full text-left px-4 py-2 text-gray-700 ${!viewingDescending ? 'bg-gray-200 cursor-not-allowed' : 'hover:bg-gray-100'
-                                }`}
-                            disabled={!viewingDescending}
-                        >
-                            Date ↑
-                        </button>
-                    </div>
-                )
-                }
-            </div>
-        </div >
+            <button
+                onClick={() => handleSelect(viewingDescending ? OCCASION_SORTS.DATE_ASCENDING : OCCASION_SORTS.DATE_DESCENDING)}
+                className="py-2 px-4 bg-gray-100 border border-orange-400 bg-white rounded-md shadow-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent flex items-center"
+            >
+                <span className="md:text-xl font-bold">{viewingDescending ? 'Date ↓' : 'Date ↑'}</span>
+            </button>
+        </div>
     );
 }
