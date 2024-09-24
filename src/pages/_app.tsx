ef637a8895d327b654a85330cadb89ed8d0a964b
Austin Lindenberg <austin@appsumo.com>
2024-09-23 19:14:58 -0700

    style: simplify main element styling in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     <SessionProvider session={session}>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <main className="flex-grow flex items-center justify-center w-full border-2 border-green-400">
+        <main className="flex-grow flex w-full">
           <Component {...pageProps} />
         </main>
       </div>
