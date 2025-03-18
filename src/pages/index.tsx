29355107bd0fea864d92eb604d03ad457d499fe4
Austin Lindenberg <austin@appsumo.com>
2025-03-17 20:44:37 -0700

    feat: add occasion alert icon to OccasionsPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -201,6 +201,9 @@ export default function OccasionsPage({ initialOccasions }: { initialOccasions: 
               <div className="flex justify-between h-16">
                 <div className="flex">
                   <div className="flex-shrink-0 flex items-center">
+                    <svg className="h-8 w-8 text-orange-500 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
+                    </svg>
                     <span className="text-2xl font-bold text-orange-500">Occasion Alerts</span>
                   </div>
                 </div>
